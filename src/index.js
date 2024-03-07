2743bf77ff38c76d6010f0832efb0fd5e5a29c3e
ShawnFrost26 <sb262202@gmail.com>
2024-03-07 16:50:33 +0530

    session 3

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ const app = express();
 require("dotenv").config({ path: "src/.env" });
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+const cors = require("cors");
 
+app.use(cors());
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
 
 mongoose
   .connect(MONGODB_URI)
---
+++ src/models/Task.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        unique: true,
+        required: true,
+    },
+    description:{
+        type: String,
+        required: true,
+    },
+    priority:{
+        type: String,
+        enum:["Low", "Medium", "High"],
+        default: "Low",
+    },
+    isCompleted: {
+        type: Boolean,
+        default: "false",
+    },
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+    },
+},{
+    timestamps: true,
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task;
